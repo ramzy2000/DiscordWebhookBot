53d3f459852538acd1a5b2f2db96d5fb7c0d9997
cduffy-tri <caseyduffy00@gmail.com>
2026-07-04 10:40:55 -0500

    modify DeathWebhook to support image URL's

--- src/main/java/org/cduffy/discordWebhookPlugin/Webhook/DiscordDeathWebhook.java
+++ src/main/java/org/cduffy/discordWebhookPlugin/Webhook/DiscordDeathWebhook.java
@@ -1,8 +1,52 @@
 package org.cduffy.discordWebhookPlugin.Webhook;
 
+import org.bukkit.entity.Player;
+
 public class DiscordDeathWebhook extends DiscordWebhook {
+
+    public String imgUrl;
     public DiscordDeathWebhook(String webhook)
     {
         super(webhook);
+    }
+
+    public void SetImgUrl(String imgUrl)
+    {
+        this.imgUrl = imgUrl;
+    }
+
+    public String buildDeathPayload(Player player, String deathMessage)
+    {
+        String jsonPayload = """
+                {
+                   "username": "Minecraft Death Bot",
+                   "embeds": [
+                     {
+                       "title": "☠️ Player Death",
+                       "description": "%s",
+                       "color": 16711680,
+                       "image": {
+                         "url": "%s"
+                       },
+                       "fields": [
+                         {
+                           "name": "Player",
+                           "value": "%s",
+                           "inline": true
+                         },
+                         {
+                           "name": "World",
+                           "value": "world",
+                           "inline": true
+                         }
+                       ]
+                     }
+                   ]
+                 }
+            """;
+
+        String name = player.getName();
+
+        return String.format(jsonPayload, deathMessage, this.imgUrl, name);
     }
 }
